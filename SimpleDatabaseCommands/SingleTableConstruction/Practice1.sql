8a2b1f42ed6ff9701e8271896003cdd7f6263e3f
JordanNitta <Jordannittarodrigues@gmail.com>
2023-07-07 11:03:48 -0700

    Working on single table construction

---
+++ SimpleDatabaseCommands/SingleTableConstruction/Practice1.sql
@@ -0,0 +1,9 @@
+-- The following CREATE TABLE statement creates a table called 'company' that consists of the company_id as the primary key, the company_name, and the address.
+
+-- CREATE TABLE company(
+-- company_id INT PRIMARY KEY,
+-- company_name TEXT (100),
+-- address VARCHAR (100)
+-- );
+
+-- Identify the line of code that would generate an error in this CREATE TABLE statement. 
